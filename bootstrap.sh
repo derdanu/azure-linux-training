db0695956e0c0bb58881b167cee98bd15fcedc62
Daniel Falkner <dafalkne@microsoft.com>
2016-09-06 14:42:42 +0200

    generate dummyfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 wget -O /tmp/master.tar.gz https://github.com/derdanu/azure-linux-training/archive/master.tar.gz
 tar xfvz /tmp/master.tar.gz -C /opt
+for i in $(seq -w 1 9999) ; do cat /opt/azure-linux-training-master/files/ADUMMY > /opt/azure-linux-training-master/files/A${i};done; 
 echo "####################################################################################" > /etc/motd
 echo "# The Training files are located in the /opt/azure-linux-training-master directory #" >> /etc/motd
 echo "####################################################################################" >> /etc/motd
