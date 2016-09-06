084670b83123a945d36a6110ff9f532ae7a6bc7a
Daniel Falkner <dafalkne@microsoft.com>
2016-09-06 16:02:07 +0200

    generate more files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,7 @@
 #/bin/bash
 wget -O /tmp/master.tar.gz https://github.com/derdanu/azure-linux-training/archive/master.tar.gz
 tar xfvz /tmp/master.tar.gz -C /opt
-for i in $(seq -w 1 9999) ; do cat /opt/azure-linux-training-master/files/ADUMMY > /opt/azure-linux-training-master/files/A${i};done; 
-for i in $(seq -w 1 3 9999) ; do cat /opt/azure-linux-training-master/files/ADUMMY >> /opt/azure-linux-training-master/files/A${i};done; 
-for i in $(seq -w 1 4 9999) ; do cat /opt/azure-linux-training-master/files/ADUMMY >> /opt/azure-linux-training-master/files/A${i};done;
-for i in $(seq -w 1 9 9999) ; do cat /opt/azure-linux-training-master/files/ADUMMY >> /opt/azure-linux-training-master/files/A${i};done;
+for i in A B C D E; do for j in 1 3 4 9; do for k in $(seq -w 1 ${j} 9999) ; do /opt/azure-linux-training-master/files/ADUMMY >> /opt/azure-linux-training-master/files/${i}${k};done; done; done;
 rm /opt/azure-linux-training-master/files/ADUMMY
 echo "####################################################################################" > /etc/motd
 echo "# The Training files are located in the /opt/azure-linux-training-master directory #" >> /etc/motd
